e14aca2b923f4874d1dcdea66befdc3cc3dc7489
Anikmajumder <53091562+Anikmajumder@users.noreply.github.com>
2021-05-20 12:08:34 +0600

    add more feature on middleware

---
+++ Back-End/Errors/ApiError.cs
@@ -0,0 +1,21 @@
+using System.Text.Json;
+
+namespace Back_End.Errors
+{
+    public class ApiError
+    {
+        public ApiError(int errorCode, string errorMessage, string errorDetails=null)
+        {
+            ErrorCode = errorCode;
+            ErrorMessage = errorMessage;
+            ErrorDetails = errorDetails;
+        }
+        public int ErrorCode{get;set;}
+        public string ErrorMessage { get; set; }
+        public string ErrorDetails { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
--- Back-End/Middlewares/ExecptionMiddleware.cs
+++ Back-End/Middlewares/ExecptionMiddleware.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Net;
 using System.Threading.Tasks;
+using Back_End.Errors;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Back_End.Middlewares
@@ -9,25 +12,49 @@ namespace Back_End.Middlewares
     {
         private readonly RequestDelegate next;
         private readonly ILogger<ExecptionMiddleware> logger;
-        public ExecptionMiddleware(RequestDelegate next, ILogger<ExecptionMiddleware> logger)
+        private readonly IHostEnvironment env;
+
+        public ExecptionMiddleware(RequestDelegate next, ILogger<ExecptionMiddleware> logger,
+        IHostEnvironment env)
         {
+            this.env = env;
             this.logger = logger;
             this.next = next;
 
 
         }
-        public async Task Invoke(HttpContext context)
+    public async Task Invoke(HttpContext context)
+    {
+        try
         {
-            try
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            ApiError response;
+            HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+            String message;
+            var exceptionType = ex.GetType();
+            if(exceptionType == typeof(UnauthorizedAccessException))
             {
-                await next(context);
+                statusCode = HttpStatusCode.Forbidden;
+                message = "You are not authorized";
+            }else{
+                statusCode = HttpStatusCode.InternalServerError;
+                message = "Some unknown error occured";
             }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, ex.Message);
-                context.Response.StatusCode = 500;
-                await context.Response.WriteAsync(ex.Message);
+            
+            if(env.IsDevelopment()){
+                response = new ApiError((int)statusCode,ex.Message,ex.StackTrace.ToString());
             }
+            else{
+                response = new ApiError((int)statusCode,message);
+            }
+            logger.LogError(ex, ex.Message);
+            context.Response.StatusCode = 500;
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(response.ToString());
         }
     }
+}
 }
